42fded50c2208ce881392712d10c004dc7545c91
Krishna_D <dkrishna051997@gmail.com>
2023-07-16 16:48:01 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [user, setUser]=useState({name:"", email:"", password:""});
+
+  function addUser(){
+    console.log(user)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <form onSubmit={addUser}>
+        Name <input type="text" onChange={()=>setUser(...user, user.name)}></input>
+        Email <input type="email"  onChange={()=>setUser(...user, user.email)}></input>
+        Password <input type="password"  onChange={()=>setUser(...user, user.password)}></input>
+        <button>Submit</button>
+       </form>
     </div>
   )
 }
